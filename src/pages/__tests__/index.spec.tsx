2a5e56a112a33da52a343ad85ee064c24c3b8e0c
shmy <pandahao123>
2020-01-07 09:59:07 +0800

    test docker

--- src/pages/__tests__/index.spec.tsx
+++ src/pages/__tests__/index.spec.tsx
@@ -2,6 +2,6 @@ import React from 'react'
 import { render } from '@testing-library/react'
 import Index from '../index'
 
-test('renders without casing', () => {
+test('renders without casing!', () => {
   render(<Index />)
 })
