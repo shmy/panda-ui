ecbe55d517d14512b88fe266ec6c8859abfe9d85
shmy <pandahao123>
2020-01-07 14:08:34 +0800

    test sonar

--- sonar-project.js
+++ sonar-project.js
@@ -2,6 +2,9 @@ const sonarqubeScanner = require('sonarqube-scanner')
 sonarqubeScanner({
   serverUrl: 'http://52.82.10.96:9000',
 	options: {
+		'sonar.projectKey': 'panda-ui',
+		'sonar.projectName': 'panda-ui',
+		'sonar.sourceEncoding': 'UTF-8',
 		'sonar.sources': '.',
 		'sonar.inclusions': 'src/**' // Entry point of your code
 	}
